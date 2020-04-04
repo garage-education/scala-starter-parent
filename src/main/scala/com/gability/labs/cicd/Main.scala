1a0467511d1b4de958dd82ab56ba031210fd285b
Ahmed Hassanien <ahmed_hassanien@epam.com>
2020-04-04 04:59:11 +0200

    Added scaladoc and generated a report for it in the maven site

--- src/main/scala/com/gability/labs/cicd/Main.scala
+++ src/main/scala/com/gability/labs/cicd/Main.scala
@@ -1,12 +1,27 @@
 package com.gability.labs.cicd
 
+/**
+ * The main object for our application.
+ */
 object Main extends App {
+
+  /**
+   * This function counts the words in a given string.
+   * @param s represents the input string we are going t count its words.
+   * @return integer representing the word count in the input string.
+   */
   def workCount(s:String): Int = {
     s match {
       case str if isEmpty(str) => 0
       case str => str.split("\\W+").length
     }
   }
+
+  /**
+   * This function checks if the input string is not null or empty.
+   * @param x represents the input string we are going to check.
+   * @return true if the input string is null or empty otherwise returns false.
+   */
   def isEmpty(x: String) = x == null || x.trim.isEmpty
 }
 
